33228209e816ea3f45b95cf90805c46e67b3db19
GraceBuri <grcbuuri@gmail.com>
2023-06-24 10:08:33 -0700

    add second part of the project

---
+++ main.c
@@ -0,0 +1,48 @@
+#include "monty.h"
+/**
+ * main - my monty code intepreter
+ * @argc: number of arguments from user
+ * @argv: monty file
+ * Return: 0 a success
+ */
+user_input input = {0, NULL, 0};
+
+int main(int argc, char *argv[])
+{
+        char *lineptr = NULL;
+        char **tokens;
+        FILE *file;
+        size_t size = 0;
+        ssize_t content = 1;
+        unsigned int line_number = 0;
+        stack_t *stack = NULL;
+
+        if (argc != 2)
+        {
+                fprintf(stderr, "USAGE: monty file\n");
+                exit(EXIT_FAILURE);
+        }
+
+        file = fopen(argv[1], "r");
+        if (!file)
+        {
+                fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+                exit(EXIT_FAILURE);
+        }
+
+        while ((content = getline(&lineptr, &size, file)) != -1)
+        {
+                tokens = tokenize(lineptr);
+                execute(tokens, &stack, line_number);
+                free(tokens);
+                line_number++;
+                input.num_tok = 0;
+                input.arg = NULL;
+	}
+        input.arg = NULL;
+        free(lineptr);
+        free_stack(&stack);
+        fclose(file);
+
+        return (0);
+}
---
+++ montyfunc1.c
@@ -0,0 +1,98 @@
+#include "monty.h"
+/**
+ * push - push an element to the stack
+ * @stack: double pointer to the stack
+ * @line_number: line opcode is searched
+ * Return: void
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+        int n;
+        char *arg = input.arg;
+
+        if (!arg)
+        {
+                fprintf(stderr, "L%u: usage push integer\n", line_number);
+                return;
+        }
+        n = atoi(arg);
+        addnode(stack, n);
+}
+
+
+/**
+ * pall - print all elements of the stack
+ * @stack: pointer to the head of the stack
+ * @line_number: line opcode is searched
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+        stack_t *current = *stack;
+
+        (void)line_number;
+        while (current)
+        {
+                printf("%d\n", current->n);
+                current = current->next;
+        }
+}
+/**
+ * addnode - add a new node to top of the stack
+ * @stack: pointer to the stack
+ * @n: value to be added
+ */
+void addnode(stack_t **stack, int n)
+{
+        stack_t *newnode = malloc(sizeof(stack_t));
+
+        if (!newnode)
+        {
+                fprintf(stderr, "Error: malloc failed\n");
+                exit(EXIT_FAILURE);
+        }
+        newnode->n = n;
+        newnode->prev = NULL;
+        newnode->next = *stack;
+
+        if (*stack != NULL)
+        {
+                (*stack)->prev = newnode;
+		newnode->prev = NULL;
+        }
+
+        *stack = newnode;
+}
+/**
+ * free_stack - function to free doubly linked list
+ * @head: head of the stack
+ */
+void free_stack(stack_t **head)
+{
+
+        stack_t *temp;
+        stack_t *current = *head;
+
+
+        while (current != NULL)
+        {
+                temp = current;
+                current = current->next;
+                free(temp);
+        }
+        *head = NULL;
+}
+/**
+ * pint - a function that prints top value of the stack
+ * @stack: a pointer to head of the doubly linked list
+ * @line_number: the current working line number of file
+*/
+void pint(stack_t **stack, unsigned int line_number)
+{
+        (void) line_number;
+        if ((*stack) == NULL)
+        {
+                fprintf(stderr, "empty stack\n");
+                return;
+        }
+        printf("%d\n", (*stack)->n);
+}
