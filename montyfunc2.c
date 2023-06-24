f3341474aa36c94e3c8693f152909ef6bb17b960
Only1allan <kariukiallan850@gmail.com>
2023-06-24 23:08:26 +0300

    fixed betty

--- montyfunc2.c
+++ montyfunc2.c
@@ -50,7 +50,7 @@ void swap(stack_t **stack, unsigned int line_number)
 */
 void add(stack_t **head, unsigned int line_number)
 {
-	 if (*head == NULL || (*head)->next == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
