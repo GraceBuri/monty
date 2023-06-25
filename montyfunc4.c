2ac2d35f776fc4b4d55da322ccc85fb72a2655f0
Only1allan <kariukiallan850@gmail.com>
2023-06-25 23:02:14 +0300

    made more changes

--- montyfunc4.c
+++ montyfunc4.c
@@ -17,7 +17,7 @@ void pchar(stack_t **head, unsigned int line_number)
 	}
 	if (tmp->n > 127 || tmp->n < 0)
 	{
-		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%c\n", tmp->n);
