0d5aac8a72188e546de98382fb0df95bcfc23422
Only1allan <kariukiallan850@gmail.com>
2023-06-25 20:12:36 +0300

    made more changes

--- montyfunc2.c
+++ montyfunc2.c
@@ -68,7 +68,7 @@ void sub(stack_t **head, unsigned int line_number)
 {
 	if (*head == NULL || (*head)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	(void) line_number;
